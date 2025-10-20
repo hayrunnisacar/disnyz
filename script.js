f0b65a0d0583f4c316ea829634877ce91fd26d7b
MadhviH <hettiaramadhvi@gmail.com>
2025-10-20 14:20:23 +0200

    Ajout de l'animation

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //Mettez vos codes ici, comme ca on garde l'ordre
+//Animation page d'accueil 
+//Je récupère tous mes éléments du HTML pour les manipuler
+const castle = document.getElementById('castle');
+const intro = document.getElementById('intro-animation');
+const main = document.getElementById('main');
+
+//je lance mon délai, comme dans mon css, mon chateau a une opacité de 0, pour le faire apparaître progressivement je mets ici l'opacité à 1. 
+setTimeout(() => {
+castle.style.opacity = '1';
+castle.style.transform = 'translateY(0)';
+}, 500);
+
+//une fois l'animation terminée, je cache l'animation avec mon chateau pour faire apparaître le contenu de ma page. 
+setTimeout(() => {
+intro.style.display = 'none';
+document.body.style.background = 'none';
+main.style.opacity = '1';
+}, 8500);
+
+
 // GRAPHIQUE 1 - MICKEY
 
 
