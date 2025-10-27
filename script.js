d19c14f14c5d77a5934d29225f7eca089c9b9f4f
MadhviH <hettiaramadhvi@gmail.com>
2025-10-27 15:12:57 +0100

    Ajout de l'animation du compteur en js + petites modification du css

--- script.js
+++ script.js
@@ -273,4 +273,54 @@ fetch('data.json').then(function(response) {
     });
 
     }); 
-});
+
+
+    // ----- ANIMATION DU COMPTEUR -----
+    //J'utilise la bibliothèque GSAP et le pluggin ScrollTrigger.
+  // On attend que le DOM soit complètement chargé
+  document.addEventListener("DOMContentLoaded", function() {
+
+    //Je sélectionne l'élement de mon HTML qui contient mon compteur donc "counter" <div class="counter">0</div>. L'animation sera faite à partir de counter. 
+    const counter = document.querySelector(".counter");
+  
+    //Je charge mon fichier data.json pour pouvoir aller récupérer les données qui m'intéressent dans le fichier pour pouvoir ensuite les manipuler en javascript. 
+    fetch('data.json').then(function(response) {
+    response.json().then(function(data) {
+    // console.log(data);  
+
+        //Comme dans mon compteur je veux afficher la somme de tous les Worldwide Boxoffice, je dois faire un calcul qui additionne toutes les données de cette colonnes. 
+        //Je crée une variable totalRecette dans lequel : reduce parcourt tout le tableau data. s est une valeur initiale et f correspond à l'élement qu'on va parcourir dans le tableau. Je pars de 0 et avec f.recette, je parcours "recette" et j'additionne toutes les valeurs "recette".
+        const totalRecette = data.reduce((s, f) => s + f.recette, 0);
+
+        //je crée un objet d'une valeur de 0 pour que mon compteur parte de 0 et pour que GSAP puisse réussir à manipuler ma valeur.
+        const obj = { valeur: 0 };
+  
+        //À partir de là, je choisis les réglages de mon animation. 
+        //Je dis à GSAP de manipuler l'objet donc la ligne de code juste au dessus.
+        gsap.to(obj, {
+        //Valeur finale : somme de la colonne recette. 
+          valeur: totalRecette,
+        //Propriété GSAP pour la durée de mon animation 
+          duration: 4,
+        //Type d'animation 
+          ease: "power1.in",
+         
+        // à chaque petit changement de la valeur pendant l'animation j'affiche dans le compteur
+          onUpdate: function() {
+            //J'arrondis le résultat final avec la fonction Math.floor pour que pendant l'animation, le compteur affiche pas les nombres à virgule. 
+            counter.textContent = Math.floor(obj.valeur);
+          },
+
+          // J'ajoute la fonction ScrollTrigger pour que l'animation ne commence que quand on arrive au niveau du compteur. 
+          scrollTrigger: {  
+            trigger: counter,     
+            start: "top center",    
+            //Je joue une seule fois l'animation  
+            toggleActions: 'play none none none'
+        }
+        });
+        });
+        })
+        });
+    });
+
